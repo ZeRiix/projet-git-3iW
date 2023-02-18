bed4a26f486ea2f6f9069f79b3367e86fed1b799
ZeRiix <willflo46@gmail.com>
2023-02-18 01:40:55 +0100

    [UPDATE]: config major routes for app

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,56 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import InitPage from "@/views/NavBarComponent.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/connexion'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
+    path: '/bde/',
+    component: InitPage,
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/bde/home'
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: 'save',
+        component: () => import('@/views/SavePage.vue')
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        path: 'document',
+        component: () => import('@/views/DocumentPage.vue')
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: 'account',
+        component: () => import('@/views/AccountPage.vue')
+      },
+      {
+        path: 'home',
+        component: () => import('@/views/EventsPage.vue')
+      },
+      {
+        path: 'calendar',
+        component: () => import('@/views/CalendarPage.vue') 
       }
     ]
+  },
+  {
+    path: '/connexion',
+    name: "connexion",
+    component: () => import('@/views/WelcomePage.vue')
+  },
+  {
+    path: '/identification',
+    name: "identification",
+    component: () => import('@/views/ConnexionPage.vue')
+  },
+  {
+    path: '/inscription',
+    name: "inscription",
+    component: () => import('@/views/InscriptionPage.vue')
   }
 ]
 
